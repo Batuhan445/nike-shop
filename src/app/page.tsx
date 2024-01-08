d4a862432424bc24c2755b8274ce1e0d52561b94
Batuhan445 <neomatrix44@hotmail.com>
2024-01-08 17:33:15 +0300

    started design early

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
-import Image from 'next/image'
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      Hero
+    </section>
+    <section className="padding">
+      PopularProducts
+    </section>
+    <section className="padding">
+      SuperQuality
+    </section>
+    <section className="padding">
+      Services
+    </section>
+    <section className="padding">
+      SpecialOffer
+    </section>
+    <section className="padding bg-pale-blue">
+      CustomerReviews
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      Subscribe
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      Footer
+    </section>
+  </main>
+)
 
-export default function Home() {
-  return (
-    <main>
-      hello world
-    </main>
-  )
-}
+export default App
