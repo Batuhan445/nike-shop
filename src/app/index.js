a3293bf516e7e7aae7f49a6331a29199ff83895e
Batuhan445 <neomatrix44@hotmail.com>
2024-01-09 15:29:08 +0300

    imported sections

---
+++ src/app/index.js
@@ -0,0 +1,20 @@
+import Hero from "../sections/Hero"
+import PopularProducts from "../sections/PopularProducts"
+import CustomerReviews from "../sections/CustomerReviews"
+import Services from "../sections/Services"
+import Subscribe from "../sections/Subscribe"
+import Footer from "../sections/Footer"
+import SuperQuality from "../sections/SuperQuality"
+import SpecialOffer from "../sections/SpecialOffer"
+
+
+export {
+    Hero,
+    PopularProducts,
+    CustomerReviews,
+    Services,
+    Subscribe,
+    Footer,
+    SuperQuality,
+    SpecialOffer
+}
