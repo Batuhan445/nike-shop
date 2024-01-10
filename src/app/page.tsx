51c53076f47960dadc2329c49ec71609fda1689b
Batuhan445 <neomatrix44@hotmail.com>
2024-01-10 16:01:37 +0300

    import problem solved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,31 @@
+import {CustomerReviews, Footer, Hero, PopularProducts, Services, SpecialOffer, Subscribe, SuperQuality } from "./index"
+
 const App = () => (
   <main className="relative">
-    Nav
+    {/* <Nav/> */}
     <section className="xl:padding-1 wide:padding-r padding-b">
-      Hero
+      <Hero/>
     </section>
     <section className="padding">
-      PopularProducts
+      <PopularProducts/>
     </section>
     <section className="padding">
-      SuperQuality
+      <SuperQuality/>
     </section>
     <section className="padding">
-      Services
+      <Services/>
     </section>
     <section className="padding">
-      SpecialOffer
+      <SpecialOffer/>
     </section>
     <section className="padding bg-pale-blue">
-      CustomerReviews
+      <CustomerReviews/>
     </section>
     <section className="padding-x sm:py-32 py-16 w-full">
-      Subscribe
+      <Subscribe/>
     </section>
     <section className="bg-black padding-x padding-t pb-8">
-      Footer
+      <Footer/>
     </section>
   </main>
 )
