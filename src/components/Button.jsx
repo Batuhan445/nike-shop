2da79988dc8663b34442ec025eff2d72bbca5477
Batuhan445 <neomatrix44@hotmail.com>
2024-04-05 15:21:21 +0300

    icon problem fix

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,11 +5,11 @@ const Button = ({label, iconURL}) => {
     <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
         {label}
 
-        <Image 
+        {iconURL && <Image 
         src={iconURL} 
         alt="arrow right icon" 
         className="ml-2 rounded-full w-5 h-5" 
-        />
+        />}
         </button>
   )
 }
