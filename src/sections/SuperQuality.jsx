6dd795b2ea3db5fde5ca34ed9d86fecaa27fb021
Batuhan445 <neomatrix44@hotmail.com>
2024-04-05 15:28:03 +0300

    super quality done

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,6 +1,42 @@
+import Button from "@/components/Button"
+import Image from "next/image"
+import { shoe8 } from "../../public/images"
+
 const SuperQuality = () => {
   return (
-    <div>SuperQuality</div>
+    <section id="about-us" className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container">
+      <div className="flex flex-1 flex-col">
+      
+        <h2 className="font-palanquin text-4xl capitalize lg:max-w-lg font-bold">
+          We provide You
+          <span className="text-coral-red"> Super</span>
+          <span className="text-coral-red"> Quality</span> Shoes
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to elevate your experience, providing you with unmatched
+          quality, innovation, and a touch of elegance.
+        </p>
+
+        <p className='mt-6 lg:max-w-lg info-text'>
+          Our dedication to detail and excellence ensures your satisfaction
+        </p>
+        <div className="mt-11">
+        <Button label="View details"/>
+        </div>
+      </div>
+
+      <div className="flex-1 justify-center items-center flex">
+        <Image
+        src={shoe8}
+        alt="shoe8"
+        width={570}
+        height={522}
+        className="object-contain"
+        />
+      </div>
+
+    </section>
   )
 }
 
