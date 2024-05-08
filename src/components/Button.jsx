35163e739cf6fceddd707b470fba3760e31da32e
Batuhan445 <neomatrix44@hotmail.com>
2024-05-08 15:59:39 +0300

    subscribe button added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,13 @@
 import Image from "next/image"
 
-const Button = ({label, iconURL, backgroundColor, borderColor, textColor}) => {
+const Button = ({label, iconURL, backgroundColor, borderColor, textColor, fullWidth}) => {
   return (
     <button
       className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
       ${backgroundColor
           ? `${backgroundColor} ${textColor} ${borderColor}`
           : "bg-coral-red text-white border-coral-red"
-      } rounded-full w-full"}`}
+      } rounded-full ${fullWidth && "w-full"}"}`}
     >
         {label}
 
