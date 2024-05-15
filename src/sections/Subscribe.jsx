2b1e7f6a40445b88ef5bbb49b9bc42bf57e78a2f
Batuhan445 <neomatrix44@hotmail.com>
2024-05-15 16:44:07 +0300

    mobile text fix

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -4,7 +4,7 @@ const Subscribe = () => {
   return (
     <section className="max-container flex justify-between items-center max-lg:flex-col gap-10">
 
-      <h3 className="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold">Sign up for
+      <h3 className="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold max-sm:text-center">Sign up for
         <span className="text-coral-red"> Updates </span>& Newsletter
       </h3>
 
